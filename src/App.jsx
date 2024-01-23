2104b84138ed0bef09b20d85600ae47b526ae730
Christopher <mwanikigachina@gmail.com>
2024-01-23 17:09:14 +0300

    updates on userId

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { Auth } from "./components/auth";
-import { db } from './config/firebase';
-import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { db, auth, storage } from './config/firebase';
+import { getDocs, 
+  collection, 
+  addDoc, 
+  deleteDoc, 
+  doc, 
+  updateDoc } from 'firebase/firestore';
+import { ref, uploadBytes } from "firebase/storage";
 
 export default function App(){
   const [movieList, setMovieList] = useState([]);
@@ -15,6 +21,9 @@ export default function App(){
   //Update Movie Title State
   const [ updatedTitle, setUpdatedTitle ] = useState('');
 
+  //file upload state
+  const [ fileUpload, setFileUpload] = useState(null);
+
   const moviesCollectionRef = collection(db, 'movies');
 
   const onSubmitMovie = async() => {
@@ -22,7 +31,8 @@ export default function App(){
       await addDoc(moviesCollectionRef, {
         title: newMovieTitle, 
         releaseDate: newReleaseDate,
-        awardReceived: isNewMovieAward
+        awardReceived: isNewMovieAward,
+        userId: auth?.currentUser?.uid
       });
     } catch (err){
       console.error(err);
@@ -39,23 +49,35 @@ export default function App(){
     await updateDoc(movieDoc, { title: updatedTitle });
   }
 
-  useEffect(() => {
-    const getMovieList = async() => {
-      try{
-        const data = await getDocs(moviesCollectionRef);
-        const filteredData = data.docs.map((doc)=> ({
-          ...doc.data(), 
-          id: doc.id
-        }))
-        //console.log(filteredData);
-        setMovieList(filteredData);
-      } catch(err){
-        console.error(err);
-      }
-    };
+  const getMovieList = async() => {
+    try{
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc)=> ({
+        ...doc.data(), 
+        id: doc.id
+      }))
+      //console.log(filteredData);
+      setMovieList(filteredData);
+    } catch(err){
+      console.error(err);
+    }
+  };
 
+
+  useEffect(() => {
+    
     getMovieList();
   }, [onSubmitMovie]);
+
+  const uploadFile = async() => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   
   return (
     <div className="app">
@@ -96,6 +118,14 @@ export default function App(){
             <button onClick={() => updateMovieTitle(movie.id)}> Update Title </button>
           </div>
         ))}
+      </div>
+
+      <div className="main">
+          <input 
+            type="file" 
+            onChange={(e) => setFileUpload(e.target.files[0])}
+          />
+          <button onClick={uploadFile}> Upload File </button>
       </div>
     </div>
   )
